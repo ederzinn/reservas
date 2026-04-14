374e00ac1d4b84b97127abbc7f563d8cb4133ae4
ederzinn <ederdetrabalho@gmail.com>
2026-04-14 16:33:51 -0300

    added login dtos

---
+++ src/main/java/com/eder/reservas/dtos/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.eder.reservas.dtos;
+
+public record LoginRequest(String email, String password) {
+}
---
+++ src/main/java/com/eder/reservas/dtos/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.eder.reservas.dtos;
+
+public record LoginResponse(String token) {
+}
