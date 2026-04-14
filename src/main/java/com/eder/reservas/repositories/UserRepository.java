b6e6f1c4dcb2d6526c8bad0a372e6e16883032e8
ederzinn <ederdetrabalho@gmail.com>
2026-04-14 17:03:26 -0300

    added findByEmail to UserRepository

--- src/main/java/com/eder/reservas/repositories/UserRepository.java
+++ src/main/java/com/eder/reservas/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.eder.reservas.repositories;
 import com.eder.reservas.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
