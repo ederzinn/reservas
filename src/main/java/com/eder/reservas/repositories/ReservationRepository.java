75cab81ec0170ea04778174f67ea714adf0aac18
ederzinn <ederdetrabalho@gmail.com>
2026-04-19 15:11:44 -0300

    added findAllBuUser method to ReservationRepository

--- src/main/java/com/eder/reservas/repositories/ReservationRepository.java
+++ src/main/java/com/eder/reservas/repositories/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.eder.reservas.repositories;
 
 import com.eder.reservas.domain.reservation.Reservation;
+import com.eder.reservas.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    List<Reservation> findAllByUser(User user);
 }
