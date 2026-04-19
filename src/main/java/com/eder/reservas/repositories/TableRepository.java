2213c3e2a12937e447687c2b23da6bb360d16390
ederzinn <ederdetrabalho@gmail.com>
2026-04-19 14:29:40 -0300

    added findByNumber method to TableRepository

--- src/main/java/com/eder/reservas/repositories/TableRepository.java
+++ src/main/java/com/eder/reservas/repositories/TableRepository.java
@@ -3,7 +3,9 @@ package com.eder.reservas.repositories;
 import com.eder.reservas.domain.table.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TableRepository extends JpaRepository<Table, UUID> {
+    Optional<Table> findByNumber(Integer number);
 }
