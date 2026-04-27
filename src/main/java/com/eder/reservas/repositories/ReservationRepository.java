588b2c45474b474ea6d53b2b08e52fc48f57b490
ederzinn <ederdetrabalho@gmail.com>
2026-04-27 14:55:16 -0300

    added existsByTableAndTimestamp method to ReservationRepository

--- src/main/java/com/eder/reservas/repositories/ReservationRepository.java
+++ src/main/java/com/eder/reservas/repositories/ReservationRepository.java
@@ -1,12 +1,16 @@
 package com.eder.reservas.repositories;
 
 import com.eder.reservas.domain.reservation.Reservation;
+import com.eder.reservas.domain.table.Table;
 import com.eder.reservas.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
     List<Reservation> findAllByUser(User user);
+    boolean existsByTableAndTimestamp(Table table, LocalDateTime reservationTimestamp);
 }
