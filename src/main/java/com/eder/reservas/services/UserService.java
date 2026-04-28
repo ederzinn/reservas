4996c77714aff212331454f24f7c2272c9593b44
ederzinn <ederdetrabalho@gmail.com>
2026-04-28 12:22:16 -0300

    created UserService

---
+++ src/main/java/com/eder/reservas/services/UserService.java
@@ -0,0 +1,43 @@
+package com.eder.reservas.services;
+
+import com.eder.reservas.domain.user.User;
+import com.eder.reservas.repositories.UserRepository;
+import com.eder.reservas.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private TokenService tokenService;
+
+    public User register(User user) {
+        if(userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already used");
+        }
+
+        User newUser = new User();
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        newUser.setRole(user.getRole());
+
+        return userRepository.save(newUser);
+    }
+
+    public String authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Email not found"));
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Incorrect password");
+        }
+
+        return tokenService.generateToken(user);
+    }
+}
